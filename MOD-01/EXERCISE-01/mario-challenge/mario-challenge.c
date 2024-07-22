ed4957f037dcca879073d7ca36e6d0ae3bb1e8f6
Thiago Xavier <thiagox58@gmail.com>
2024-07-22 12:22:26 -0300

    feat: mario-challenge implemented

---
+++ MOD-01/EXERCISE-01/mario-challenge/mario-challenge.c
@@ -0,0 +1,46 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int get_positive_int(void);
+void do_wall(int);
+
+int main(void)
+{
+  int height = get_positive_int();
+  do_wall(height);
+}
+
+int get_positive_int(void)
+{
+  int n;
+  do
+  {
+    n = get_int("Height: ");
+  } while (n < 1 || n > 8);
+  return n;
+}
+
+void do_wall(int height)
+{
+  for (int i = 0; i < height; i++)
+  {
+    for (int j = 0; j < height - i - 1; j++)
+    {
+      printf(" ");
+    }
+
+    for (int k = 0; k <= i; k++)
+    {
+      printf("#");
+    }
+
+    printf(" ");
+
+    for (int l = 0; l <= i; l++)
+    {
+      printf("#");
+    }
+
+    printf("\n");
+  }
+}
